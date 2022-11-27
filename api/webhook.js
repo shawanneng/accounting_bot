b208a73d584e979635d3c1611188f142a3f6cc71
Liuwa113 <no-rival@foxmail.com>
2022-11-27 11:54:47 +0800

    调整使用说明

--- api/webhook.js
+++ api/webhook.js
@@ -83,13 +83,15 @@ module.exports = async (request, response) => {
 
       if (text === '使用说明' || at === '使用说明') {
         outMsg = `<i>使用说明</i>\n
-<b>发送指令<pre>查询实时U价格</pre> 可查实时USDT价格</b>\n
-<b>发送指令<pre>清空账本</pre> 可清空记账本重新开始</b>\n
-<b>发送指令+RMB如<pre>+100</pre> 使用记账加100</b>\n
-<b>发送指令设置费率+费率如<pre>设置费率7.25</pre>设置当前记账费率</b>\n
-<b>发送指令-RMB如<pre>-100</pre> 使用记账减100</b>\n
-<b>发送指令下发U如<pre>下发100</pre> 使用记账减100u</b>\n
-<b>直接发送冷钱包U地址 可查询实时余额</b>\n`;
+<b>发送指令<pre>查询实时U价格</pre> 可查实时USDT价格</b>
+<b>发送指令<pre>U100</pre> 可查100UDST折合人民币价格</b>
+<b>发送指令<pre>CNY100</pre> 可查100人民币汇算UDST价格</b>
+<b>发送指令<pre>清空账本</pre> 可清空记账本重新开始</b>
+<b>发送指令+RMB如<pre>+100</pre> 使用记账加100</b>
+<b>发送指令设置费率+费率如<pre>设置费率7.25</pre>设置当前记账费率</b>
+<b>发送指令-RMB如<pre>-100</pre> 使用记账减100</b>
+<b>发送指令下发U如<pre>下发100</pre> 使用记账减100u</b>
+<b>直接发送冷钱包U地址 可查询实时余额</b>`;
         await bot.sendMessage(id, outMsg, {
           parse_mode: 'HTML',
           ...options,
@@ -219,7 +221,7 @@ module.exports = async (request, response) => {
       //根据U换算人民币
       let rateReg = new RegExp(/^(U|CNY)/);
       let price = text.toLocaleUpperCase();
-      let curAmount = price.replace(reg, '').trim();
+      let curAmount = price.replace(rateReg, '').trim();
       if (rateReg.test(price) && Number.isFinite(+curAmount)) {
         let [rateItem] = await getOk();
         if (price.includes('U')) {
