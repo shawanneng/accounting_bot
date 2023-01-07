c0ae93980d7ce9006f8726eb71566c3c2fe915cc
Liuwa113 <1073104684@qq.com>
2023-01-07 19:34:03 +0800

    修复查询

--- api/webhook.js
+++ api/webhook.js
@@ -140,11 +140,11 @@ module.exports = async (request, response) => {
         at === '查询实时U价格' ||
         new RegExp(/^\w{1,2}\d{1,5}$/).test(text)
       ) {
-        const head = new RegExp(/\w{1,2}/).exec(text)?.[0].toLocaleUpperCase();
+        const head = new RegExp(/\w{1,2}/).exec(text)?.[0];
         let end = new RegExp(/\d{1,5}/).exec(text)?.[0];
         end = end > 10 ? end : 100;
-        let selectPaymentMethod = '';
-        let methodName = '';
+        let selectPaymentMethod = 'bank';
+        let methodName = '银行卡';
         switch (true) {
           case head?.includes('y'):
             selectPaymentMethod = 'bank';
@@ -165,10 +165,6 @@ module.exports = async (request, response) => {
 
           default:
             break;
-        }
-
-        if (!selectPaymentMethod) {
-          return;
         }
         let list = await getOk(selectPaymentMethod, end);
         const outList = list.map(
